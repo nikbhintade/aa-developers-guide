528aa7209c9903a4df56fa09b6f8b688127b15a2
Nikhil Bhintade <29760727+nikbhintade@users.noreply.github.com>
2024-09-27 09:45:31 +0000

    complete coverage: simple account

--- src/SimpleAccount.sol
+++ src/SimpleAccount.sol
@@ -2,15 +2,54 @@
 pragma solidity 0.8.24;
 
 import {BaseAccount} from "account-abstraction/core/BaseAccount.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "account-abstraction/core/Helpers.sol";
 import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
+import {PackedUserOperation} from "account-abstraction/interfaces/PackedUserOperation.sol";
+
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract SimpleAccount is BaseAccount {
+
+    error SimpleAccount__NotFromEntryPoint();
+    error SimpleAccount__CallFailed(bytes result);
+
     IEntryPoint private immutable i_entryPoint;
     address private immutable i_owner;
+
+    modifier onlyEntryPoint() {
+        if(msg.sender != address(i_entryPoint)){
+            revert SimpleAccount__NotFromEntryPoint();
+        }
+        _;
+    }
 
     constructor(address entryPointAddress, address owner) {
         i_entryPoint = IEntryPoint(entryPointAddress);
         i_owner = owner;
+    }
+
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
+        internal
+        view
+        override
+        returns (uint256)
+    {
+        bytes32 digest = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address messageSigner = ECDSA.recover(digest, userOp.signature);
+
+        if (messageSigner == i_owner) {
+            return SIG_VALIDATION_SUCCESS;
+        } else {
+            return SIG_VALIDATION_FAILED;
+        }
+    }
+
+    function execute(address dest, uint256 value, bytes calldata funcCallData) external onlyEntryPoint {
+        (bool success, bytes memory result) = dest.call{value: value}(funcCallData);
+        if(!success) {
+            revert SimpleAccount__CallFailed(result);
+        } 
     }
 
     function entryPoint() public view override returns (IEntryPoint) {
